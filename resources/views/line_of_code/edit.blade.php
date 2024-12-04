e8a6852740b6bb19de0c3f63878a364fbaca9725
Man-Minh-PG <manbui0207@gmail.com>
2024-12-04 09:15:26 +0700

    Process UI screen edit

--- resources/views/line_of_code/edit.blade.php
+++ resources/views/line_of_code/edit.blade.php
@@ -33,34 +33,21 @@
             {{-- <td><i class="ri-suitcase-2-line ri-22px text-danger me-4"></i><span>#191817</span></td> --}}
             <td><a href="#"> {{$lstLocDetail[0]->number_task}} </a></td>
             <td>
-              @if ($lstLocDetail[0]->status == config('common.new'))
-                <span class="badge bg-label-warning rounded-pill">New</span>
-              @elseif ($lstLocDetail[0]->status == config('common.inProgress'))
-                <span class="badge bg-label-info rounded-pill">In Progress</span>
-              @elseif ($lstLocDetail[0]->status == config('common.completed'))
-                <span class="badge bg-label-success rounded-pill">Completed</span>
-              @elseif ($lstLocDetail[0]->status == config('common.close'))
-                <span class="badge bg-label-secondary rounded-pill">Close</span>
-              @endif
               <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
-                @foreach ($lstStatus as $key => $child)
-                <option selected="">Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                @foreach ($lstStatus as $key => $status)
+                  <option value="{{$key}}" @if($lstLocDetail[0]->status == $key) selected="true" @endif> {{ $status }} </option>
                 @endforeach
               </select>
             </td>
-            </td>
-            <td> <input type="text" class="form-control" id="basic-default-fullname" name="fileChange" value="{{$lstLocDetail[0]->file_change}}">  File</td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="fileChange" value="{{$lstLocDetail[0]->file_change}}"></td>
             <td> <input type="text" class="form-control" id="basic-default-fullname" name="php" value="{{$lstLocDetail[0]->php}}">  </td>
             <td> <input type="text" class="form-control" id="basic-default-fullname" name="js" value="{{$lstLocDetail[0]->js}}">  </td>
             <td> <input type="text" class="form-control" id="basic-default-fullname" name="css" value="{{$lstLocDetail[0]->css}}">  </td>
             <td> <input type="text" class="form-control" id="basic-default-fullname" name="tpl" value="{{$lstLocDetail[0]->tpl}}">  </td>
             <td> <input type="text" class="form-control" id="basic-default-fullname" name="total" value="{{$lstLocDetail[0]->total}}">  </td>
-            <td> <input type="text" class="form-control" id="basic-default-fullname" name="createAt" value="{{$lstLocDetail[0]->created_at}}">  </td>
+            <td> {{$lstLocDetail[0]->created_at}}</td>
             <td> <input type="text" class="form-control" id="basic-default-fullname" name="branh" value="{{$lstLocDetail[0]->branch}}">  </td>
-            <td> <input type="text" class="form-control" id="basic-default-fullname" name="notes" value="{{$lstLocDetail[0]->notes}}">  </td>
+            <td> <textarea class="form-control h-px-100" id="exampleFormControlTextarea1" name="notes" placeholder="{{$lstLocDetail[0]->notes}}"></textarea>  </td>
           </tr>
         </tbody>
 
@@ -90,25 +77,21 @@
             {{-- <td><i class="ri-suitcase-2-line ri-22px text-danger me-4"></i><span>#191817</span></td> --}}
             <td><a href="#"> {{$child->number_task}} </a></td>
             <td>
-              @if ($child->status == config('common.new'))
-                <span class="badge bg-label-warning rounded-pill">New</span>
-              @elseif ($child->status == config('common.inProgress'))
-                <span class="badge bg-label-info rounded-pill">In Progress</span>
-              @elseif ($child->status == config('common.completed'))
-                <span class="badge bg-label-success rounded-pill">Completed</span>
-              @elseif ($child->status == config('common.close'))
-                <span class="badge bg-label-secondary rounded-pill">Close</span>
-              @endif
+              <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                @foreach ($lstStatus as $key => $status)
+                  <option value="{{$key}}" @if($child->status == $key) selected="true" @endif> {{ $status }} </option>
+                @endforeach
+              </select>
             </td>
-            <td> {{$child->file_change}} File</td>
-            <td> {{$child->php}} </td>
-            <td> {{$child->js}} </td>
-            <td> {{$child->css}} </td>
-            <td> {{$child->tpl}} </td>
-            <td> {{$child->total}} </td>
-            <td> {{$child->created_at}} </td>
-            <td> {{$child->branch}} </td>
-            <td> {{$child->notes}} </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="fileChange" value="{{$lstLocDetail[0]->file_change}}"></td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="php" value="{{$lstLocDetail[0]->php}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="js" value="{{$lstLocDetail[0]->js}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="css" value="{{$lstLocDetail[0]->css}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="tpl" value="{{$lstLocDetail[0]->tpl}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="total" value="{{$lstLocDetail[0]->total}}">  </td>
+            <td> {{$lstLocDetail[0]->created_at}}</td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="branh" value="{{$lstLocDetail[0]->branch}}">  </td>
+            <td> <textarea class="form-control h-px-100" id="exampleFormControlTextarea1" name="notes" value="{{$lstLocDetail[0]->notes}}"></textarea>  </td>
           </tr>
           @endforeach
         </tbody>
--- resources/views/line_of_code_beer/edit.blade.php
+++ resources/views/line_of_code_beer/edit.blade.php
@@ -4,12 +4,19 @@
  <!-- Hoverable Table rows -->
  <!-- <hr class="my-12" /> -->
  <div class="card">
-    <h5 class="card-header">Edit line of code : - time: 2024-11 </h5>
+  <!-- Notifications -->
+  <div class="card-body">
+    <h5 class="mb-0">Infomation task # {{$lstLocDetail[0]->number_task}}</h5>
+    <span class="card-subtitle">Date time:
+      <a href="javascript:void(0);" class="notificationRequest"> {{$lstLocDetail[0]->created_at}} </a></span>
+    <div class="error"></div>
+  </div>
     <div class="table-responsive text-nowrap">
       <table class="table table-hover">
         <thead>
           <tr>
             <th>Parent task</th>
+            <th>Source</th>
             <th>Status</th>
             <th>File_change</th>
             <th>PHP</th>
@@ -19,32 +26,42 @@
             <th>Total</th>
             <th>Create at</th>
             <th>Branch</th>
-            <th>Note</th>
+            <th>Notes</th>
           </tr>
         </thead>
         <tbody class="table-border-bottom-0">
           <tr>
-            <td><a href="">#1989273</a></td>
-            <td>Done</td>
-            <td>11 File</td>
-            <td>97</td>
-            <td>67</td>
-            <td>32</td>
-            <td>32</td>
-            <td>132</td>
-            <td>2024:11:22 11:22:11</td>
-            <th>Branch123</th>
-            <td>note 123</td>
+            {{-- <td><i class="ri-suitcase-2-line ri-22px text-danger me-4"></i><span>#191817</span></td> --}}
+            <td><a href="#"> {{$lstLocDetail[0]->number_task}} </a></td>
+            <td> {{$lstLocDetail[0]->source_type}} </td>
+            <td>
+              <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                @foreach ($lstStatus as $key => $status)
+                  <option value="{{$key}}" @if($lstLocDetail[0]->status == $key) selected="true" @endif> {{ $status }} </option>
+                @endforeach
+              </select>
+            </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="fileChange" value="{{$lstLocDetail[0]->file_change}}"></td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="php" value="{{$lstLocDetail[0]->php}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="js" value="{{$lstLocDetail[0]->js}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="css" value="{{$lstLocDetail[0]->css}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="tpl" value="{{$lstLocDetail[0]->tpl}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="total" value="{{$lstLocDetail[0]->total}}">  </td>
+            <td> {{$lstLocDetail[0]->created_at}}</td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="branh" value="{{$lstLocDetail[0]->branch}}">  </td>
+            <td> <textarea class="form-control h-px-100" id="exampleFormControlTextarea1" name="notes" placeholder="{{$lstLocDetail[0]->notes}}"></textarea>  </td>
           </tr>
         </tbody>
 
       </table>
       <hr class="my-12" />
       <!-- Config child table -->
+      @isset($lstLocDetail[0]->childTasks)
       <table class="table table-hover">
         <thead>
           <tr>
-            <th>child task</th>
+            <th>Child task</th>
+            <th>Source</th>
             <th>Status</th>
             <th>File_change</th>
             <th>PHP</th>
@@ -54,56 +71,41 @@
             <th>Total</th>
             <th>Create at</th>
             <th>Branch</th>
-            <th>Note</th>
+            <th>Notes</th>
           </tr>
         </thead>
         <tbody class="table-border-bottom-0">
+          @foreach ($lstLocDetail[0]->childTasks as $child)
           <tr>
             {{-- <td><i class="ri-suitcase-2-line ri-22px text-danger me-4"></i><span>#191817</span></td> --}}
-            <td><input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe" value="#1989273"></td>
+            <td><a href="#"> {{$child->number_task}} </a></td>
+            <td><a href="#"> {{$child->source_type}} </a></td>
             <td>
               <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
-                <option selected="">Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                @foreach ($lstStatus as $key => $status)
+                  <option value="{{$key}}" @if($child->status == $key) selected="true" @endif> {{ $status }} </option>
+                @endforeach
               </select>
             </td>
-            <td><input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe" value="11 File"></td>
-            <td><input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe" value="97"></td>
-            <td><input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe" value="67"></td>
-            <td><input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe" value="32"></td>
-            <td><input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe" value="32"></td>
-            <td><input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe" value="132"></td>
-            <td><input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe" value="2024:11:22 11:22:11"></td>
-            <td>
-              <textarea class="form-control h-px-100" id="exampleFormControlTextarea1" placeholder="Branch..."></textarea>
-            </td>
-            <td>
-              <textarea class="form-control h-px-100" id="exampleFormControlTextarea1" placeholder="Comments here..."></textarea>
-            </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="fileChange" value="{{$lstLocDetail[0]->file_change}}"></td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="php" value="{{$lstLocDetail[0]->php}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="js" value="{{$lstLocDetail[0]->js}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="css" value="{{$lstLocDetail[0]->css}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="tpl" value="{{$lstLocDetail[0]->tpl}}">  </td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="total" value="{{$lstLocDetail[0]->total}}">  </td>
+            <td> {{$lstLocDetail[0]->created_at}}</td>
+            <td> <input type="text" class="form-control" id="basic-default-fullname" name="branh" value="{{$lstLocDetail[0]->branch}}">  </td>
+            <td> <textarea class="form-control h-px-100" id="exampleFormControlTextarea1" name="notes" value="{{$lstLocDetail[0]->notes}}"></textarea>  </td>
           </tr>
+          @endforeach
         </tbody>
                      
       
       </table>
+      @endisset
       <!-- Config child table -->
     </div>
   </div>
-
-  <div class="col-12">
-    <div class="card mb-6">
-      <div class="row row-bordered g-0">
-        <div class="col-lg-4 p-6">
-          <button type="button" class="btn rounded-pill btn-outline-primary waves-effect">
-            <span class="tf-icons ri-checkbox-circle-line ri-16px me-1_5"></span>Save
-          </button>
-          </div>
-        </div>
-      </div>
-  </div>
-  
- 
   <!--/ Hoverable Table rows -->
-   
+
 @stop()
