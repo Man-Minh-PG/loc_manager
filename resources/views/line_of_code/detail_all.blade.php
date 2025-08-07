615925c1f642c0d0417142ab0d7413cd6109ec1b
Man-Minh-PG <manbui0207@gmail.com>
2025-08-07 11:21:53 +0700

    fix bug get data modal pw

--- resources/views/line_of_code/detail_all.blade.php
+++ resources/views/line_of_code/detail_all.blade.php
@@ -196,7 +196,7 @@
                             <div>
                                 <!-- Gọi hàm callAjaxShowPopup với 3 tham số -->
                               <button type="button" class="btn btn-sm btn-outline-primary"
-                                  onclick="callAjaxShowPopup('{{$child->id}}','{{ $child->number_task }}', 1 ,'{{  $child->project_type }}', '{{  $child->source_type }}')">
+                                  onclick="callAjaxShowPopup('{{$child->id}}','{{ $child->number_task }}', 2 ,'{{  $child->project_type }}', '{{  $child->source_type }}')">
                                   <i class="ri-more-2-line"></i>
                               </button>
 
